eeb46a99f003add7b5310ad1197bd660fcc7fbb7
senyoudev <younesmeskafe2018@gmail.com>
2025-03-13 00:28:36 +0000

    Set hostname from code

--- main.go
+++ main.go
@@ -15,24 +15,46 @@ import (
  	switch os.Args[1] {
  	case "run":
 		run()
+	case "child":
+		child()
 	default:
  		panic("Bad command")
  	}
  }
 
  func run() {
- 	fmt.Printf("Running %v\n", os.Args[2:])
- 
- 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
- 	cmd.Stdin = os.Stdin
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
  	cmd.Stdout = os.Stdout
  	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
  		Cloneflags: syscall.CLONE_NEWUTS,
  	}
- 
+
+
  	if err := cmd.Run(); err != nil {
  		fmt.Printf("Error running command: %v\n", err)
+ 	}
+ }
+
+ func child() {
+ 	fmt.Printf("Running %v as %d in child\n", os.Args[2:], os.Getpid())
+
+	// Set hostname
+ 	if err := syscall.Sethostname([]byte("oracleContainer")); err != nil {
+ 		fmt.Printf("Failed to set hostname: %v\n", err)
+ 		os.Exit(1)
+ 	}
+ 
+ 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+ 	cmd.Stdin = os.Stdin
+ 	cmd.Stdout = os.Stdout
+ 	cmd.Stderr = os.Stderr
+ 
+ 	if err := cmd.Run(); err != nil {
+ 		fmt.Printf("Error running command in child: %v\n", err)
  	}
  }
